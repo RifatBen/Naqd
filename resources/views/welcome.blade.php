2801a861a7c4f02bda7a9393e045d9ad6ec1868f
RifatBen <benmebarek.raf@gmail.com>
2018-02-20 23:50:48 +0100

    test
    ee

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mounir
                 </div>
 
                 <div class="links">
