1ec3546c5462e8a3878b705204f378c6e3c90cf1
AllZoth <atb.mounir.28@gmail.com>
2018-02-20 22:56:34 +0100

    wololo
    oIo

--- resources/views/connexion.blade.php
+++ resources/views/connexion.blade.php
@@ -1,9 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<meta charset="utf-8">
+	<title>NAQD</title>
 </head>
 <body>
+<<<<<<< HEAD
+
+	<p>Mounir is a good person</p>
+
+=======
+<<<<<<< HEAD
+		<p>Mounir nta gros pd ya zah</p>
+
+		<h1>Mounir pourquoi es tu si beau ?</h1>
+=======
 		<p></p>
+>>>>>>> 84773b1ac0e748960d4159f30763b385b2bd6648
+>>>>>>> b1a1c5f49b9e140d902e3e4c6ea13960f296645f
 </body>
 </html>
