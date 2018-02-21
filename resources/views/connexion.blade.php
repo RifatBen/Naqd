e6c82796a8eace5a62a97fb9416dd55e3e695569
AllZoth <atb.mounir.28@gmail.com>
2018-02-21 17:49:44 +0100

    lolo

---
+++ resources/views/connexion.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+	<link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
+	<title>NAQD</title>
+</head>
+<body>
+
+	<div class="col s6 offset-s3">
+		<h3 class="center-align">Connexion</h3>
+	</div>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+</body>
+</html>
