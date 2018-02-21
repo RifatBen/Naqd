c14518fbe6c5d44644ee18074d7966a592389116
RifatBen <benmebarek.raf@gmail.com>
2018-02-21 17:52:52 +0100

    Ajout d'un layout folder

--- resources/views/connexion.blade.php
+++ resources/views/connexion.blade.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
-	<link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
-	<title>NAQD</title>
-</head>
-<body>
+@extends('layouts.master')
 
-	<div class="col s6 offset-s3">
+@section('content')
+
+<div class="col s6 offset-s3">
 		<h3 class="center-align">Connexion</h3>
-	</div>
+</div>
 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-</body>
-</html>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+	<link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
+	<title>NAQD</title>
+</head>
+<body>
+
+
+		@yield('content')
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+</body>
+</html>
