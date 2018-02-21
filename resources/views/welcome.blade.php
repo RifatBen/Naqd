e72fcdbdf3462fa34e189ff55a0946bb5fe18424
AllZoth <atb.mounir.28@gmail.com>
2018-02-21 17:17:29 +0100

    wololo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Mounir
+                    Laravel
                 </div>
 
                 <div class="links">
