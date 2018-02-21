a73a3d0369c1e719597f0d493907abae6b6a9c46
RifatBen <benmebarek.raf@gmail.com>
2018-02-22 00:36:38 +0100

    Création d'InscriptionRequest

--- app/Http/Controllers/InscriptionController.php
+++ app/Http/Controllers/InscriptionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Request\InscriptionRequest;
 
 class InscriptionController extends Controller {
 	public function __construct() {
@@ -13,30 +14,8 @@ class InscriptionController extends Controller {
 		return view('auths.inscription');
 	}
 
-	public function store(Request $request) {
-		$this->validate(request(),[
-			'nom' => 'required|max:255',
-			'prenom' => 'required|max:255',
-			'email' => 'required|max:255|email',
-			'mot_de_passe' => 'required|min:8|max:32|confirmed',
-			'genre' => 'required|max:255',
-			'date' => 'required|max:255',
-			'pays' => 'required|max:255',
-			'niveau' => 'required|max:255',
-			'etab' => 'required|max:255'
-		]);
-
-		User::create([
-			'nom' => request('nom'),
-			'prenom' => request('prenom'),
-			'email' => request('email'),
-			'mot_de_passe' => bcrypt(request('mot_de_passe')),
-			'genre' => request('genre'),
-			'date' => request('date'),
-			'pays' => request('pays'),
-			'niveau' => request('niveau'),
-			'etab' => request('etab')
-		]);
+	public function store(InscriptionRequest $request) {
+		$request->persist();
 
 		return redirect('/');
 	}
---
+++ app/Http/Requests/InscriptionRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class InscriptionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+           'nom' => 'required|max:255',
+            'prenom' => 'required|max:255',
+            'email' => 'required|max:255|email',
+            'mot_de_passe' => 'required|min:8|max:32|confirmed',
+            'genre' => 'required|max:255',
+            'date' => 'required|max:255',
+            'pays' => 'required|max:255',
+            'niveau' => 'required|max:255',
+            'etab' => 'required|max:255'
+        ];
+    }
+
+
+    public function persist(){
+
+            User::create(
+            $this->only(['nom' ,'prenom', 'email','mot_de_passe','genre' ,'date', 'pays','niveau' , 'etab'])
+        );
+
+
+    }
+}
