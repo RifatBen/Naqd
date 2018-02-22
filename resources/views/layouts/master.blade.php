4b93c62375d47c6e3c47dc13adea162fac3cb6d9
RifatBen <benmebarek.raf@gmail.com>
2018-02-22 12:41:38 +0100

    test

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,15 +11,14 @@
 	<title>NAQD</title>
 </head>
 <body>
-      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-      
- @yield('content')
-	
+
+	@yield('content')
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
 	<script>
 		$(document).ready(function() {
-    $('select').material_select();
-});
+    		$('select').material_select();
+		});
 	</script>
 </body>
 </html>
